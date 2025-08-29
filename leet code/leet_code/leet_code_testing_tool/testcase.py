1feea1c480c91cf4b15e34df5f0fd1f54bb1bb4c
shubhamjoshi-shiv <64223308+shubhamjoshi-shiv@users.noreply.github.com>
2025-08-29 16:56:27 +0530

    update: refactor testing code to improve result comparison

--- leet code/leet_code/leet_code_testing_tool/testcase.py
+++ leet code/leet_code/leet_code_testing_tool/testcase.py
@@ -1,22 +1,32 @@
+
+def compare_results(result, output_param, input_param, comparison_level):
+    match comparison_level:
+        case 0:
+            if result != output_param:
+                print('tc failed','expected ->',output_param,result,'<- got', input_param)
+            else:
+                print('tc passed')
+        case 1:
+            if result != output_param:
+                print('tc failed','expected ->',output_param,result,'<- got', input_param, sep='\n')
+            else:
+                print('tc passed')
+
 def run_test_case():
-            s = Solution()
+    s = Solution()
+    comparison_level = 0
+
     
-            # Example 1
-            result = s.trap(height = [0,1,0,2,1,0,1,3,2,1,2,1])
-            output_param = 6
-            if result != output_param:
-                print('expected result -',output_param)
-                print('got result -',result)
-            else:
-                print('tc passed')
-            
-            # Example 2
-            result = s.trap(height = [4,2,0,3,2,5])
-            output_param = 9
-            if result != output_param:
-                print('expected result -',output_param)
-                print('got result -',result)
-            else:
-                print('tc passed')
-            
+    # Example 1
+    result = s.maxArea(height = [1,8,6,2,5,4,8,3,7])
+    input_param = 'height = [1,8,6,2,5,4,8,3,7]'
+    output_param = 49
+    compare_results(result, output_param, input_param, comparison_level)
+        
+    # Example 2
+    result = s.maxArea(height = [1,1])
+    input_param = 'height = [1,1]'
+    output_param = 1
+    compare_results(result, output_param, input_param, comparison_level)
+        
 run_test_case()
